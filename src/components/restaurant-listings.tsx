48bf207778ffd22bbc7750b324d973e0afc8673c
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2024-06-07 15:24:13 +0700

    feat: Add pagination component to restaurant listings
    This commit adds a new pagination component to the restaurant listings page. The pagination component will be used to navigate through the list of restaurants. It is implemented as a separate component called "Pagination" and is imported in the "restaurant-listings.tsx" file. This addition will enhance the user experience by allowing them to easily navigate through multiple pages of restaurant listings.

--- src/components/restaurant-listings.tsx
+++ src/components/restaurant-listings.tsx
@@ -14,6 +14,7 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import type { SearchParams } from "../global.type";
 import type { Schema } from "../lib/firebase/firestore/schema";
+import Pagination from "./pagination";
 
 const RestaurantItem = ({
 	restaurant,
@@ -112,6 +113,7 @@ export default function RestaurantListings({
 					<RestaurantItem key={restaurant.id} restaurant={restaurant} />
 				))}
 			</ul>
+			<Pagination />
 		</article>
 	);
 }
